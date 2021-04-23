dd868fc8bdee02329fa2bb5a72718d3e692d8bf1
Sergij <AdonGor@users.noreply.github.com>
2021-04-23 18:52:04 +0300

    ...

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,6 +3,6 @@ import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
  export default function ImageLoader() {
     return (
-      <Loader type="Bars" color="#00BFFF" height={80} width={80} />
+      <Loader type="Puff" color="#00BFFF" height={80} width={80} />
     );
 }
